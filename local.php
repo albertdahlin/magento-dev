9425171bddbaf0d5bbd8506ec275540038637acb
Albert Dahlin <info@albertdahlin.com>
2015-01-17 14:35:09 +0100

    Deleted local.php

--- local.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('/var/www/dahbug.php');
-ini_set('display_errors', 1);
-if ($config->getProject() !== 'deval') {
-    $_SERVER['MAGE_IS_DEVELOPER_MODE'] = 1;
-}
-$config->setLogFile('/var/www/log');
