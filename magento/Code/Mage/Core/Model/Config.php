334962ceab09eee29773db3de2bf622a3a02507e
Albert Dahlin <info@albertdahlin.com>
2015-01-18 02:02:15 +0100

    Added Mage core config override

---
+++ magento/Code/Mage/Core/Model/Config.php
@@ -0,0 +1,10 @@
+<?php
+include('config-original191.php');
+class Mage_Core_Model_Config
+ extends original_mage_core_model_config
+{
+    public function __construct($sourceData = null)
+    {
+        parent::__construct($sourceData);
+    }
+}
