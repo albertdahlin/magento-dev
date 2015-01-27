6a9830bbfb15ad9f1f56ecc9f3629cef3b9097d4
Albert Dahlin <info@albertdahlin.com>
2015-01-27 07:43:32 +0100

    Added php docs

--- magento.php
+++ magento.php
@@ -1,16 +1,61 @@
 <?php
 
-class dahl_dev 
+/**
+ * This class bootstraps the dev app.
+ * 
+ * @package dahl_dev
+ * @copyright Copyright (C) 2015 Albert Dahlin
+ * @author Albert Dahlin <info@albertdahlin.com> 
+ * @license GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
+ */
+class dahl_dev
 {
+    /**
+     * Singleton instance of dahl_dev
+     * 
+     * @var dahl_dev
+     * @access protected
+     */
     static protected $_instance;
     
+    /**
+     * Dev configuration data object.
+     * 
+     * @var Varien_Object
+     * @access protected
+     */
     protected $_config;
 
+    /**
+     * Initializes dev app.
+     * 
+     * @static
+     * @access public
+     * @return void
+     */
     static public function init()
     {
         self::$_instance = new dahl_dev();
     }
 
+    /**
+     * Retrieve the config object.
+     * 
+     * @static
+     * @access public
+     * @return Varien_Object
+     */
+    static public function getConfig()
+    {
+        return self::$_instance->_config;
+    }
+
+    /**
+     * Constructor method. Initializes config.
+     * 
+     * @access public
+     * @return void
+     */
     public function __construct()
     {
         if (!$this->_initConfig()) {
@@ -20,17 +65,25 @@ class dahl_dev
         $this->_initSetup();
     }
 
-    static public function getConfig()
-    {
-        return self::$_instance->_config;
-    }
-
+    /**
+     * Writes a log message to the log file.
+     * 
+     * @param string $string 
+     * @access protected
+     * @return void
+     */
     protected function log($string)
     {
         $logFile = $this->_config->getLogFile();
         file_put_contents($logFile, 'DAHL_DEV: ' . $string . "\n", FILE_APPEND);
     }
 
+    /**
+     * Initialize configuration. Reads the settings from .json files.
+     * 
+     * @access protected
+     * @return boolean
+     */
     protected function _initConfig()
     {
         $mageRoot    = $_SERVER['DOCUMENT_ROOT'];
@@ -51,6 +104,13 @@ class dahl_dev
         return true;
     }
 
+    /**
+     * Reads Configuration Files and stores the data in the
+     * _config property.
+     * 
+     * @access protected
+     * @return void
+     */
     protected function _readConfigFiles()
     {
         $config   = $this->_config;
@@ -74,6 +134,13 @@ class dahl_dev
         }
     }
 
+    /**
+     * Reads and parses a .json file.
+     * 
+     * @param string $filename
+     * @access protected
+     * @return array $config
+     */
     protected function _readConfigJson($filename)
     {
         if (!file_exists($filename)) {
@@ -91,6 +158,12 @@ class dahl_dev
         return $config;
     }
 
+    /**
+     * Initializes setup from configuration options.
+     * 
+     * @access protected
+     * @return void
+     */
     protected function _initSetup()
     {
         $config = $this->_config;
@@ -103,4 +176,7 @@ class dahl_dev
 }
 
 
+/**
+ * Bootstrap dev app.
+ */
 dahl_dev::init();
