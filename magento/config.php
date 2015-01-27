3bb06560da5d9b713da37af00d59d1fb62249a29
Manne Busk <manne@webbhuset.se>
2015-01-28 00:07:25 +0100

    Updated Autologin - Added mode for logging in but ignoring password.

--- magento/config.php
+++ magento/config.php
@@ -6,7 +6,27 @@
 
 $config->addData(
     array(
+        /**
+         * Magento Developer Mode
+         */
         'developer_mode'    => true,
+
+        /**
+         * Log file for combining all magento logs
+         */
         'log_file'          => '/var/log/dahbug.log',
+
+        /** 
+         * Settings for autologin
+         *
+         * Modes:
+         *  auto        - Logs in automatically to the configured username on pageload.
+         *  no_passowrd - Requires no password when logging in.
+         *
+        'auto_login'        => array (
+            'username'  => 'admin',
+            'mode'      => 'no_password',
+        )
+        */
     )
 );
