3a8ba5ec3f6f307098d422a6aeb716d9c3a4fba8
Albert Dahlin <info@albertdahlin.com>
2015-01-27 07:33:06 +0100

    Removed the need to set 'DAHL_MAGENTO' from webserver

--- dev.php
+++ dev.php
@@ -1,10 +1,17 @@
 <?php
 define('DAHL_DEVROOT', dirname(__file__));
+
+/**
+ * Put your local includes or changes in local.php
+ */
 if (file_exists(DAHL_DEVROOT . '/local.php')) {
     include(DAHL_DEVROOT . '/local.php');
 }
 
-if (isset($_SERVER['DAHL_MAGENTO'])) {
+/**
+ * Check if the request is for a Magento site.
+ */
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/app/Mage.php')) {
     include(DAHL_DEVROOT . '/magento.php');
 } else {
     include(dirname(__file__) . '/default.php');
