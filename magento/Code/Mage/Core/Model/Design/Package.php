6578d8e564957145b8c428cc9713356e9ea0ea42
Albert Dahlin <info@albertdahlin.com>
2015-02-10 10:27:13 +0100

    Fixed v1.7.0.2 compatibility issue

--- magento/Code/Mage/Core/Model/Design/Package.php
+++ magento/Code/Mage/Core/Model/Design/Package.php
@@ -1,7 +1,17 @@
 <?php
 
 
-include('package-original191.php');
+$versionInfo = Mage::getVersionInfo();
+if ($versionInfo['major'] == 1) {
+    switch ($versionInfo['minor']) {
+        case 7:
+            include('package-original1702.php');
+            break;
+        case 9:
+            include('package-original191.php');
+            break;
+    }
+}
 class Mage_Core_Model_Design_Package
  extends original_mage_core_model_design_package
 {
