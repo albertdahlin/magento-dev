fd7cf7c4758c1ff97425492aa6361ca37aac2946
Albert Dahlin <info@albertdahlin.com>
2015-02-12 08:00:59 +0100

    Added path building funciton

--- dev.php
+++ dev.php
@@ -1,18 +1,23 @@
 <?php
 define('DAHL_DEVROOT', dirname(__file__));
 
+function buildPath() {
+    $args = func_get_args();
+    return implode(DIRECTORY_SEPARATOR, $args);
+}
 /**
  * Put your local includes or changes in local.php
  */
-if (file_exists(DAHL_DEVROOT . '/local.php')) {
-    include(DAHL_DEVROOT . '/local.php');
+if (file_exists(buildPath(DAHL_DEVROOT, 'local.php'))) {
+    include buildPath(DAHL_DEVROOT, 'local.php');
 }
+
 
 /**
  * Check if the request is for a Magento site.
  */
-if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/app/Mage.php')) {
-    include(DAHL_DEVROOT . '/magento.php');
+if (file_exists(buildPath($_SERVER['DOCUMENT_ROOT'], 'app', 'Mage.php'))) {
+    include buildPath(DAHL_DEVROOT, 'magento.php');
 } else {
-    include(DAHL_DEVROOT . '/default.php');
+    include buildPath(DAHL_DEVROOT, 'default.php');
 }
