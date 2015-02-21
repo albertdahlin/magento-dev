ef5824df9aa0bc44fdff16f1f1c81bd9455c3c9a
Albert Dahlin <info@albertdahlin.com>
2015-02-21 14:13:05 +0100

    Updated documentation

--- magento/default.php
+++ magento/default.php
@@ -6,7 +6,24 @@
  * and add your changes there.
  */
 
+/**
+ * All Magento log and exceptions will be printed to this file
+ * instead.
+ */
 $config->setLogFile('/var/www/test');
+/**
+ * Enable Magento Developer mode.
+ */
 $config->setMageDevmode(true);
-$config->setModulePath('~/modules');
+/**
+ * If you have all your modules in one dir you can set this dir
+ * and url here.
+ * Then you can load a module like this:
+ * $config->loadExternal('some-module');
+ * 
+ * This will load a module from /var/www/modules/some-module and include
+ * static fils on http://some-module.example.com
+ * 
+ */
+$config->setModulePath('/var/www/modules');
 $config->setModuleUrl('http://%s.example.com');
