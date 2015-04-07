4af3c3a4e8c5b994f7460a17eec2ace2b9222c1b
Anton Samuelsson <anton@webbhuset.se>
2015-04-07 15:17:52 +0000

    Added support for Magento 1.6

--- magento/Code/Mage/Core/Model/Design/Package.php
+++ magento/Code/Mage/Core/Model/Design/Package.php
@@ -7,6 +7,9 @@
 $versionInfo = Mage::getVersionInfo();
 if ($versionInfo['major'] == 1) {
     switch ($versionInfo['minor']) {
+        case 6:
+            include('package-original1602.php');
+            break;
         case 7:
             include('package-original1702.php');
             break;
